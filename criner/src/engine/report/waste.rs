4e31f902d547d4bb4dd01aaca3390f8b42bd8712
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-02-23 21:13:34 +0800

    First report hello world - just that alone weighs 850MB on disk

--- criner/src/engine/report/waste.rs
+++ criner/src/engine/report/waste.rs
@@ -1,14 +1,55 @@
-use crate::{error::Result, model};
-use std::path::Path;
+use crate::{error::Result, model, persistence};
+use std::path::{Path, PathBuf};
 
-pub struct Generator;
+const GENERATOR_VERSION: &str = "1.0.0";
+
+pub struct Generator {
+    pub db: persistence::Db,
+}
 
 impl Generator {
     pub fn write_files<'a>(
         &mut self,
-        _out_dir: impl AsRef<Path>,
-        _krates: impl IntoIterator<Item = model::Crate<'a>>,
+        out_dir: impl AsRef<Path>,
+        krates: impl IntoIterator<Item = (sled::IVec, model::Crate<'a>)>,
+        mut progress: prodash::tree::Item,
     ) -> Result<()> {
+        use std::os::unix::fs;
+        for (k, c) in krates.into_iter() {
+            let name = to_name(&k);
+            let c: model::Crate = c;
+            let mut p = progress.add_child(name);
+            p.init(Some(c.versions.len() as u32), Some("versions"));
+
+            for (vid, v) in c.versions.iter().enumerate() {
+                p.set((vid + 1) as u32);
+                let out_file = output_file_html(out_dir.as_ref(), name, &v);
+                std::fs::create_dir_all(out_file.parent().expect("parent dir for file"))?;
+
+                let mut marker = out_file.clone();
+                marker.set_file_name(GENERATOR_VERSION);
+                if !marker
+                    .symlink_metadata()
+                    .map(|m| m.is_file())
+                    .unwrap_or(false)
+                {
+                    generate_single_file(&out_file)?;
+                    fs::symlink(&out_file, &marker)?;
+                }
+            }
+        }
         Ok(())
     }
+}
+
+fn generate_single_file(out: &Path) -> Result<()> {
+    std::fs::write(out, "hello world").map_err(Into::into)
+}
+
+fn output_file_html(base: &Path, name: &str, version: &str) -> PathBuf {
+    base.join(name).join(version).join("index.html")
+}
+
+fn to_name(key: &sled::IVec) -> &str {
+    std::str::from_utf8(key).expect("unicode keys")
 }
--- criner/src/engine/stage/report.rs
+++ criner/src/engine/stage/report.rs
@@ -1,30 +1,34 @@
-use crate::utils::check;
 use crate::{
+    utils::check,
     engine::report,
     error::Result,
     model,
-    persistence::{Db, TreeAccess},
+    persistence::{self, TreeAccess}
 };
 use itertools::Itertools;
 use std::{path::PathBuf, time::SystemTime};
 
 pub async fn generate(
-    db: Db,
+    db: persistence::Db,
     mut progress: prodash::tree::Item,
     assets_dir: PathBuf,
     deadline: Option<SystemTime>,
 ) -> Result<()> {
     let krates = db.crates();
     let chunk_size = 500;
-    let output_dir = assets_dir.clone();
-    let mut waste_aggregator = report::waste::Generator;
-    progress.init(Some(krates.tree().len() as u32), Some("crates"));
+    let output_dir = assets_dir.join("reports");
+    let mut waste_aggregator = report::waste::Generator { db: db.clone() };
+    let waste_report_dir = output_dir.join("waste");
+
+    std::fs::create_dir_all(&waste_report_dir)?;
+    let num_crates = krates.tree().len() as u32;
+    progress.init(Some(num_crates), Some("crates"));
 
     for (cid, chunk) in krates
         .tree()
         .iter()
         .filter_map(|res| res.ok())
-        .map(|(_k, v)| model::Crate::from(v))
+        .map(|(k, v)| (k, model::Crate::from(v)))
         .chunks(chunk_size)
         .into_iter()
         .enumerate()
@@ -32,7 +36,12 @@ pub async fn generate(
         check(deadline.clone())?;
         progress.set(((cid + 1) * chunk_size) as u32);
         progress.blocked(None);
-        waste_aggregator.write_files(output_dir.join("waste"), chunk)?;
+        waste_aggregator.write_files(
+            &waste_report_dir,
+            chunk,
+            progress.add_child("waste report"),
+        )?;
     }
+    progress.set(num_crates);
     Ok(())
 }
