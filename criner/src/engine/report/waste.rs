556d98084d53cb21c1f510ed893db5a72290ab9e
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-03-08 21:17:32 +0800

    First investigation on what there is to optimize; sqlite is a huge help…
    …it's already clear there is huge potential.

--- criner/src/engine/report/waste.rs
+++ criner/src/engine/report/waste.rs
@@ -1,44 +1,134 @@
 use crate::persistence::TableAccess;
 use crate::{error::Result, model::TaskResult, persistence};
 use async_trait::async_trait;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 pub struct Generator;
 
-pub enum Report {
-    None,
+mod report {
+    use crate::{model::TaskResult, Result};
+    use async_trait::async_trait;
+    use std::path::{Path, PathBuf};
+
+    #[derive(PartialEq, Eq, Debug)]
+    pub enum Report {
+        None,
+        Version {
+            total_size_in_bytes: u64,
+            total_files: u64,
+            wasted_bytes: u64,
+            wasted_files: u64,
+            suggested_include: Option<Vec<String>>,
+        },
+    }
+
+    impl From<TaskResult> for Report {
+        fn from(result: TaskResult) -> Report {
+            match result {
+                TaskResult::ExplodedCrate {
+                    entries_meta_data,
+                    selected_entries: _,
+                } => Report::Version {
+                    total_size_in_bytes: entries_meta_data.iter().map(|e| e.size).sum(),
+                    total_files: entries_meta_data.len() as u64,
+                    wasted_bytes: 0,
+                    wasted_files: 0,
+                    suggested_include: None,
+                },
+                _ => unreachable!("need caller to assure we get exploded crates only"),
+            }
+        }
+    }
+
+    #[async_trait]
+    impl crate::engine::report::generic::Aggregate for Report {
+        fn merge(self, other: Self) -> Self {
+            other
+        }
+
+        async fn complete_all(
+            self,
+            _out_dir: PathBuf,
+            _progress: prodash::tree::Item,
+        ) -> Result<()> {
+            Ok(())
+        }
+        async fn complete_crate(
+            &mut self,
+            _out_dir: &Path,
+            _crate_name: &str,
+            _progress: &mut prodash::tree::Item,
+        ) -> Result<()> {
+            Ok(())
+        }
+    }
+
+    impl Default for Report {
+        fn default() -> Self {
+            Report::None
+        }
+    }
+
+    #[cfg(test)]
+    mod from_extract_crate {
+        use super::Report;
+        use crate::model::TaskResult;
+
+        const GNIR: &[u8] =
+            include_bytes!("../../../tests/fixtures/gnir-0.14.0-alpha3-extract_crate-1.0.0.bin");
+        const SOVRIN: &[u8] = include_bytes!(
+            "../../../tests/fixtures/sovrin-client.0.1.0-179-extract_crate-1.0.0.bin"
+        );
+        const MOZJS: &[u8] =
+            include_bytes!("../../../tests/fixtures/mozjs_sys-0.67.1-extract_crate-1.0.0.bin");
+
+        #[test]
+        fn gnir() {
+            assert_eq!(
+                Report::from(TaskResult::from(GNIR)),
+                Report::Version {
+                    total_size_in_bytes: 15216510,
+                    total_files: 382,
+                    wasted_bytes: 0,
+                    wasted_files: 0,
+                    suggested_include: None
+                },
+                "correct size and assume people are aware if includes or excludes are present"
+            );
+        }
+        #[test]
+        fn sovrin_client() {
+            assert_eq!(
+                Report::from(TaskResult::from(SOVRIN)),
+                Report::Version {
+                    total_size_in_bytes: 20283032,
+                    total_files: 479,
+                    wasted_files: 0,
+                    wasted_bytes: 0,
+                    suggested_include: None
+                },
+                "build.rs is used but there are a bunch of extra directories that can be ignored and are not needed by the build, no manual includes/excludes"
+            );
+        }
+        #[test]
+        fn mozjs() {
+            // todo: filter tests, benches, examples, image file formats, docs, allow everything in src/ , but be aware of tests/specs
+            assert_eq!(
+                Report::from(TaskResult::from(MOZJS)),
+                Report::Version {
+                    total_size_in_bytes: 161225785,
+                    total_files: 13187,
+                    wasted_files: 0,
+                    wasted_bytes: 0,
+                    suggested_include: None
+                },
+                "build.rs + excludes in Cargo.toml - this leaves a chance for accidental includes for which we provide an updated exclude list"
+            );
+        }
+    }
 }
 
-impl From<TaskResult> for Report {
-    fn from(_result: TaskResult) -> Report {
-        Report::None
-    }
-}
-
-#[async_trait]
-impl super::generic::Aggregate for Report {
-    fn merge(self, other: Self) -> Self {
-        other
-    }
-
-    async fn complete_all(self, _out_dir: PathBuf, _progress: prodash::tree::Item) -> Result<()> {
-        Ok(())
-    }
-    async fn complete_crate(
-        &mut self,
-        _out_dir: &Path,
-        _crate_name: &str,
-        _progress: &mut prodash::tree::Item,
-    ) -> Result<()> {
-        Ok(())
-    }
-}
-
-impl Default for Report {
-    fn default() -> Self {
-        Report::None
-    }
-}
+pub use report::Report;
 
 // NOTE: When multiple reports should be combined, this must become a compound generator which combines
 // multiple implementations into one, statically.
