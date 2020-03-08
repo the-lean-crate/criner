b5e74b61fd2cd2301741117a43d8cd7fa292880b
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-03-08 22:06:45 +0800

    Automatically turn on the logger in no-gui, but allow people to override it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use structopt::StructOpt;
 
 fn main() -> criner::error::Result<()> {
-    env_logger::init();
-    criner_cli::run_blocking(criner_cli::Parsed::from_args())
+    let args = criner_cli::Parsed::from_args();
+    if let Some(criner_cli::SubCommands::Mine { no_gui, .. }) = args.sub {
+        if no_gui {
+            env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+        }
+    }
+    criner_cli::run_blocking(args)
 }
