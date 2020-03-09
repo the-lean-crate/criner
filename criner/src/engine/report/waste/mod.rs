93e4546447286ea866379b8b4b6b1bbfbf42a43a
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-03-09 08:00:30 +0800

    refactor - note that it's not worth to split it off into a crate…
    …even though it will have additional dependencies.
    
    However, it's so tightly coupled with criner that without even more
    splitting work, it's not benefit to do.
    
    Besides, who needs crates on crates.io that are only useful to one
    other program?
    
    What about 'anonymous' crates, those that are part of another
    crate but not directly available on crates.io?

---
+++ criner/src/engine/report/waste/mod.rs
@@ -0,0 +1,68 @@
+use crate::persistence::TableAccess;
+use crate::{error::Result, model::TaskResult, persistence};
+use async_trait::async_trait;
+use std::path::Path;
+
+mod report;
+pub use report::Report;
+
+pub struct Generator;
+
+// NOTE: When multiple reports should be combined, this must become a compound generator which combines
+// multiple implementations into one, statically.
+#[async_trait]
+impl super::generic::Generator for Generator {
+    type Report = Report;
+    type DBResult = TaskResult;
+
+    fn name() -> &'static str {
+        "waste"
+    }
+
+    fn version() -> &'static str {
+        "1.0.0"
+    }
+
+    fn fq_result_key(crate_name: &str, crate_version: &str, key_buf: &mut String) {
+        let dummy_task = crate::engine::work::cpubound::default_persisted_extraction_task();
+        let dummy_result = TaskResult::ExplodedCrate {
+            entries_meta_data: Default::default(),
+            selected_entries: Default::default(),
+        };
+        dummy_result.fq_key(crate_name, crate_version, &dummy_task, key_buf);
+    }
+
+    fn get_result(
+        connection: persistence::ThreadSafeConnection,
+        crate_name: &str,
+        crate_version: &str,
+        key_buf: &mut String,
+    ) -> Result<Option<TaskResult>> {
+        Self::fq_result_key(crate_name, crate_version, key_buf);
+        let table = persistence::TaskResultTable { inner: connection };
+        table.get(&key_buf)
+    }
+
+    async fn generate_single_file(
+        out: &Path,
+        _crate_name: &str,
+        _crate_version: &str,
+        result: TaskResult,
+        _report: &Report,
+        _progress: &mut prodash::tree::Item,
+    ) -> Result<Self::Report> {
+        use async_std::prelude::*;
+        let report = Report::from(result);
+
+        async_std::fs::OpenOptions::new()
+            .truncate(true)
+            .write(true)
+            .create(true)
+            .open(out)
+            .await?
+            .write_all("hello world".as_bytes())
+            .await
+            .map_err(crate::Error::from)?;
+        Ok(report)
+    }
+}
