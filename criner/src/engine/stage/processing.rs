bd8be2657af73c3deb615c317b876cbadaff4651
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-05-18 08:34:46 +0800

    temporary fix for blocking cpu processors

--- criner/src/engine/stage/processing.rs
+++ criner/src/engine/stage/processing.rs
@@ -22,7 +22,9 @@ pub async fn process(
         let (tx_cpu, rx) = piper::chan(1);
         for idx in 0..cpu_bound_processors {
             let max_retries_on_timeout = 0;
-            smol::Task::blocking(
+            // TODO: Only use Task::blocking(…).detach() once that works - currently it's probably more of a bug that it doesn't
+            // because the detached future for the blocking task needs to be evaluated to work.
+            smol::Task::spawn(smol::Task::blocking(
                 work::generic::processor(
                     db.clone(),
                     processing_progress.add_child(format!("{}:CPU IDLE", idx + 1)),
@@ -35,7 +37,7 @@ pub async fn process(
                         log::warn!("CPU bound processor failed: {}", e);
                     }
                 }),
-            )
+            ))
             .detach();
         }
         tx_cpu
